d910d200f8d3526d531523600c7437ed640597de
cholhwanjung <romio4m@gmail.com>
2024-11-06 23:42:42 +0900

    feat: add argparse for gpu

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,4 @@
+import argparse
 from io import BytesIO
 
 import pandas as pd
@@ -18,11 +19,11 @@ def chunk_images(image_list, batch_size):
     for i in range(0, len(image_list), batch_size):
         return image_list[i:i + batch_size]
 
-def generate_captions_in_batches(images, batch_size):
+def generate_captions_in_batches(images, vis_processor, model, device, batch_size):
     all_captions = []
     for batch in tqdm(chunk_images(images, batch_size)):
         # Preprocess the current batch
-        processed_images = [vis_processor(Image.open(BytesIO(image))).unsqueeze(0).to("cuda") for image in batch]
+        processed_images = [vis_processor(Image.open(BytesIO(image))).unsqueeze(0).to(device) for image in batch]
         batch_images = torch.cat(processed_images, dim=0)
         
         # Generate captions for the batch
@@ -35,11 +36,11 @@ def generate_captions_in_batches(images, batch_size):
     
     return all_captions
 
-if __name__ == "__main__":
+def main(args):
     batch_size = 16
     model_type = "pretrain_opt2.7b"
     checkpoint_path = "./ckpt/base/blip2_model.pth"
-    test_data_path = "/home/charles/VLM/data/facad_test_data.parquet"
+    test_data_path = "./facad_test_data.parquet"
 
     test_data = pd.read_parquet(test_data_path)
     image_bytes = list(test_data["image_bytes"])
@@ -58,11 +59,11 @@ if __name__ == "__main__":
     vis_processor = vis_processors["eval"]
     text_processor = text_processors["eval"]
     
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
     model.to(device)
     model.eval()
 
-    captions = generate_captions_in_batches(image_bytes, batch_size=batch_size)
+    captions = generate_captions_in_batches(image_bytes, vis_processor, model, device, batch_size=batch_size)
 
     bleu = load("bleu")
     rouge = load("rouge")
@@ -82,3 +83,10 @@ if __name__ == "__main__":
     # METEOR
     meteor_score = meteor.compute(predictions=predictions, references=references)
     print("METEOR:", meteor_score["meteor"])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate model with specific GPU")
+    parser.add_argument("--gpu", type=int, default=0, help="GPU device number to use")
+    args = parser.parse_args()
+    
+    main(args)
